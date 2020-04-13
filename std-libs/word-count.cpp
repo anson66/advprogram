d8d0c536cc6979165ccc7f47901af650d49c4da0
Chau Tran <chauttm@vnu.edu.vn>
2020-04-13 17:33:06 +0700

    set and map examples

---
+++ std-libs/word-count.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main ()
+{
+    map<string,int> count;
+    string word;
+
+    while (cin >> word) {
+        count[word] += 1;
+    }
+
+    map<string, int>::iterator i;
+    for (i = count.begin(); i != count.end(); i++) {
+        cout << i->first << ":" << i->second << endl;
+    }
+
+    //C++11
+    for (auto const& x : count) {
+        cout << x.first << ':' << x.second << endl ;
+    }
+}
---
+++ std-libs/word-set.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main ()
+{
+    set<string> word_set;
+    string word;
+
+    while (cin >> word) {
+        word_set.insert(word);
+    }
+
+    set<string>::iterator i;
+    for (i = word_set.begin(); i != word_set.end(); i++)
+        cout << *i << endl;
+
+    //C++11
+    for (auto const& word : word_set) {
+        cout << word << endl;
+    }
+}
